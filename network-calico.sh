ac6484bc9babd4b7e087eea4b4fa8b5ecca0204d
贺亮 <>
2021-11-25 17:27:58 +0800

    add wating message

--- network-calico.sh
+++ network-calico.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+# 等待默认存储安装完成
+status=FAILED
+printf "waiting "
+while [ $status != "Running" ]
+do
+    sleep 2s
+    status=$(kubectl get po -A | grep calico-kube | awk '{print $4}')
+    printf "."
+done
+printf "\n"
+echo "calico install done!"
