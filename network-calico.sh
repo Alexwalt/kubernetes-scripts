ef5ac502dd5941a0e4f90d26ae90a7cdb7fee4a5
贺亮 <>
2021-11-25 17:56:09 +0800

    fix

--- network-calico.sh
+++ network-calico.sh
@@ -4,7 +4,7 @@ kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 # 等待默认存储安装完成
 status=FAILED
 printf "waiting "
-while [ $status != "Running" ]
+while [[ $status != "Running" ]]
 do
     sleep 2s
     status=$(kubectl get po -A | grep calico-kube | awk '{print $4}')
